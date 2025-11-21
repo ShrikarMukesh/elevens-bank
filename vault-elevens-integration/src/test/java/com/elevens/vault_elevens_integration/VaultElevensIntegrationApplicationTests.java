5b6dd9afc65005731163ef305d11dd4cfd21de95
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2025-11-21 21:43:46 +0530

    Introduce `vault-elevens-integration` module with initial setup; add Spring Boot configuration, Maven wrapper, basic POM dependencies, and main application entry point with corresponding test class.

---
+++ vault-elevens-integration/src/test/java/com/elevens/vault_elevens_integration/VaultElevensIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elevens.vault_elevens_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultElevensIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
